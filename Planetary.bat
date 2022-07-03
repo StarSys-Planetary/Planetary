54f0c2c84a1b34ae1b9c94c74e7c0091fe21b3fa
Masked-Idiot <blaze12768@gmail.com>
2022-07-03 08:45:44 -0400

    added remove option

--- Planetary.bat
+++ Planetary.bat
@@ -2,4 +2,8 @@
 set mode=%1
 set repo=%2
 cd "C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter"
-git clone https://github.com/StarSys-Planetary/%repo%.git
+if %mode% ==install (
+	git clone https://github.com/StarSys-Planetary/%repo%.git
+)else if %mode% ==remove (
+	rmdir /s %repo%
+)
